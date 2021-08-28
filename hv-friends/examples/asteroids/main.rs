5c75fbce89ba2a29bdea4333b9b83f71964bbe69
Shea Leffler <shea@errno.com>
2021-08-28 11:15:34 -0700

    unnecessary link forcing function call

--- hv-friends/examples/asteroids/main.rs
+++ hv-friends/examples/asteroids/main.rs
@@ -4,8 +4,6 @@ use hv_core::{conf::Conf, engine::Engine, filesystem::Filesystem};
 use hv_friends::SimpleHandler;
 
 fn main() {
-    hv_friends::link_me();
-
     let conf = Conf {
         filesystem: Filesystem::from_project_dirs(
             Path::new("examples/asteroids"),
